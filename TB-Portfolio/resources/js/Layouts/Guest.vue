83be90f5168b994c21b6e100227b33daaba8aedf
Thomas Bockhorn <historian.thomas@gmail.com>
2022-08-04 19:36:27 -0500

    Added main.js to make various parts work

--- TB-Portfolio/resources/js/Layouts/Guest.vue
+++ TB-Portfolio/resources/js/Layouts/Guest.vue
@@ -13,6 +13,11 @@ import navbar from "../Components/frontend/navbar/navbar.vue";
 export default {
     components:{
        navbar,
+    },
+    mounted(){
+        let addScript = document.createElement('script');
+        addScript.setAttribute('src', 'frontend/js/js/main.js');
+        document.head.appendChild(addScript);
     }
 }
 </script>
