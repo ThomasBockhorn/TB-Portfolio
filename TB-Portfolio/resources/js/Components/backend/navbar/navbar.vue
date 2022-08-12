d45222d26399f1e8ddb597293cf1ae47ef3c7137
Thomas Bockhorn <historian.thomas@gmail.com>
2022-08-12 12:40:47 -0500

    got rid of the profile image in back, GitHub link, and toggle setting.
    Signed-off-by: Thomas Bockhorn <historian.thomas@gmail.com>

--- TB-Portfolio/resources/js/Components/backend/navbar/navbar.vue
+++ TB-Portfolio/resources/js/Components/backend/navbar/navbar.vue
@@ -6,30 +6,6 @@
                 <a class = "menu-toggle" href = "#" @click = "sidebarToggle"><span></span></a>
                 <h1 class = "text-white p-2">Thomas Bockhorn</h1>
             </div>
-            <div class = "p-1 flex flex-row items-center">
-                <a class = "text-white p-2 mr-2 no-underline hidden md:block lg:block"
-                   href = "https://github.com/tailwindadmin/admin">Github</a>
-
-
-                <img alt = "" class = "inline-block h-8 w-8 rounded-full"
-                     src = "https://avatars0.githubusercontent.com/u/4323180?s=460&v=4">
-                <a class = "text-white p-2 no-underline hidden md:block lg:block" href = "#"
-                   onclick = "profileToggle()">Adam Wathan</a>
-                <div id = "ProfileDropDown" class = "rounded hidden shadow-md bg-white absolute pin-t mt-12 mr-1 pin-r">
-                    <ul class = "list-reset">
-                        <li><a class = "no-underline px-4 py-2 block text-black hover:bg-grey-light" href = "#">My
-                                                                                                                account</a>
-                        </li>
-                        <li><a class = "no-underline px-4 py-2 block text-black hover:bg-grey-light" href = "#">Notifications</a>
-                        </li>
-                        <li>
-                            <hr class = "border-t mx-2 border-grey-light">
-                        </li>
-                        <li><a class = "no-underline px-4 py-2 block text-black hover:bg-grey-light"
-                               href = "#">Logout</a></li>
-                    </ul>
-                </div>
-            </div>
         </div>
     </nav>
 </template>
@@ -46,7 +22,7 @@ export default {
         sidebarToggle() {
             this.show = !this.show;
             this.$emit('sidebarToggle', this.show);
-        }
+        },
     }
 }
 </script>
